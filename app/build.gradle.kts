9668eeed75d9a3ccce475dca0f128a88c2ebb22d
jaroid <imthanhhai217@gmail.com>
2024-02-18 21:26:46 +0700

    update targetSdk, version jdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.jaroidx.asyncutor"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.jaroidx.asyncutor"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
